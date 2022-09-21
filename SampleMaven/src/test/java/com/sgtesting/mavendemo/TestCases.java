593896b97c6c89970f8bd4c8e5713a8f365d8239
manju <priyankama1521@gmail.com>
2022-09-21 17:33:16 +0530

    Initial Draft

---
+++ SampleMaven/src/test/java/com/sgtesting/mavendemo/TestCases.java
@@ -0,0 +1,18 @@
+package com.sgtesting.mavendemo;
+
+import org.testng.annotations.Test;
+
+public class TestCases {
+
+	@Test
+	public static void method1()
+	{
+		System.out.println("Its method1");
+	}
+	
+	@Test
+	public static void method2()
+	{
+		System.out.println("Its method2");
+	}
+}
